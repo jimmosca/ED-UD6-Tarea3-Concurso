104643cd7a4389d203411986ec38b229734b0cf0
dmg2112 <daviddeozz@hotmail.com>
2019-01-29 10:59:55 +0100

    correccion menor

--- src/collecciones/Conciertos.java
+++ src/collecciones/Conciertos.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Conciertos {
 
-	public static <K> void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int casos = sc.nextInt();
 		for (int i = 0; i < casos; i++) {
