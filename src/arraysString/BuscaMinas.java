c7b19cf6df20138b5f4ae36328900ed892eacada
jimmosca <jaime.casado.a@gmail.com>
2019-01-29 10:24:34 +0100

    BuscaMinas finalizado

--- src/arraysString/BuscaMinas.java
+++ src/arraysString/BuscaMinas.java
@@ -1,9 +1,28 @@
 package arraysString;
 
+import java.util.Scanner;
+
 public class BuscaMinas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		int filas = in.nextInt(), columnas = in.nextInt();
+		while (filas!=0 || columnas!=0) {
+			char[][] tablero = new char[filas][columnas];
+			for (int i = 0; i < filas; i++) {
+				tablero[i] = in.next().toCharArray();
+			}
+			int posX = in.nextInt(), posY = in.nextInt();
+			if (tablero[posX-1][posY-1]=='*') {
+				System.out.println("ES BOMBA");
+			}else
+				System.out.println("NO ES BOMBA");
+			System.out.println("---");
+			
+			filas = in.nextInt(); 
+			columnas = in.nextInt();
+		}
 
 	}
 
