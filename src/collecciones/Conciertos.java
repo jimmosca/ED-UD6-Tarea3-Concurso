b1fa83c9c1294ce5111c61c160ff3d71fa9676d1
dmg2112 <daviddeozz@hotmail.com>
2019-01-29 10:59:01 +0100

    Ejercicio hecho

--- src/collecciones/Conciertos.java
+++ src/collecciones/Conciertos.java
@@ -1,9 +1,54 @@
 package collecciones;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Conciertos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static <K> void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int casos = sc.nextInt();
+		for (int i = 0; i < casos; i++) {
+			double presupuesto = sc.nextDouble();
+			int conciertos = sc.nextInt();
+			HashMap<String, Double> lista = new HashMap<String, Double>();
+			for (int j = 0; j < conciertos; j++) {
+				String nombre = sc.next();
+				Double precio = sc.nextDouble();
+				lista.put(nombre, precio);
+			}
+			String comprados = "";
+			Double minim = Double.MAX_VALUE;
+			while (presupuesto > 0) {
+				for (String key : lista.keySet()) {
+					Double cuesta = lista.get(key);
+					if (minim > cuesta) {
+						minim = cuesta;
+					}
+
+				}
+				presupuesto -= minim;
+				if (presupuesto < 0) {
+
+				} else {
+					Iterator it = lista.keySet().iterator();
+					while(it.hasNext()){
+						String llave= it.next().toString();
+						Double cuesta = lista.get(llave);
+						if (minim.equals(cuesta)) {
+							comprados += llave + " ";
+							it.remove();
+						
+						}
+					}
+					
+				}
+				minim=Double.MAX_VALUE;
+			}
+			System.out.println(comprados);
+
+		}
 
 	}
 
